c3e7e6bd0f3e2cd22d225db942f229bff091be73
fafkhe <115066487+fafkhe@users.noreply.github.com>
2023-11-17 20:05:19 +0330

    [NEW][API] getSingleUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { craeteUserDto } from './dtos/createUser.dto';
 import { Me } from 'src/decorators/me.decorators';
 import { User } from 'src/entities/User.entity';
 import { AuthGuard } from 'src/gaurds/auth.gaurd';
+import { Serialize } from 'src/interceptors/serialize.interceptors';
+import { singleUserDto } from './dtos/singleUserDto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,11 +20,19 @@ export class AuthController {
   login(@Body() body: craeteUserDto) {
     return this.authService.login(body);
   }
-  
+
   @UseGuards(AuthGuard)
   @Get('me')
   me(@Me() me: User) {
     console.log('me is called', me);
     return me;
+  }
+
+  
+  @Serialize(singleUserDto)
+  @UseGuards(AuthGuard)
+  @Get('singleuser/:id')
+  getSingleUser(@Param('id') id: number) {
+    return this.authService.findById(id);
   }
 }
